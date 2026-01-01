850b59e508cc73dba721806e61e4a3c1f4455408
Khoifee <151704994+FraN-onLine@users.noreply.github.com>
2026-01-01 17:46:17 +0800

    Add HelloWorld class with main method

---
+++ Object Oriented Program/helloworld.java
@@ -0,0 +1,6 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!, and hello reader, who is probably myself looking back at the past, Welcome back to the peaceful days, Welcome to JAVA");
+    }
+}
